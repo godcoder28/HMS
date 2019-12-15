504fface7f3e60ec56db899ea7e52afdac57736e
sg287 <shivanggupta287@gmail.com>
2019-12-15 18:28:06 +0530

    insert satements
    Signed-off-by: sg287 <shivanggupta287@gmail.com>

--- data.sql
+++ data.sql
@@ -18,4 +18,18 @@ create table if not exists rooms
 	status tinyint(1) default 0 null,
 	type varchar(20) null
 );
-
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (101, 0, 'suit');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (102, 0, 'suit');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (103, 0, 'suit');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (104, 0, 'suit');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (105, 0, 'suit');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (201, 0, 'executive');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (202, 0, 'executive');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (203, 0, 'executive');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (204, 0, 'executive');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (205, 0, 'executive');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (301, 0, 'deluxe');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (302, 0, 'deluxe');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (303, 0, 'deluxe');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (304, 0, 'deluxe');
+INSERT INTO coder01.rooms (roomn, status, type) VALUES (305, 0, 'deluxe');
